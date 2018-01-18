bcbfb842b45207b325139c43ccaa43c3e9936e10
sebsa2 <sebastien.geo94@gmail.com>
2018-01-18 17:16:28 +0100

    Added gif creation feature in post processing notebook

--- variables.py
+++ variables.py
@@ -8,9 +8,9 @@ Created on Sun Jan 14 17:11:27 2018
 
 # Conditions exterieures
 T0 = 300
-Ta = 200
-T1 = 500
-h = 100
+Ta = 250
+T1 = 400
+h = 1000
 
 # Materiau : cuivre pur par exemple
 lamb = 386 # W.m^-1.K^-1
@@ -19,16 +19,16 @@ rho = 8960 # kg.m^-3
 diff = lamb / (rho*cp) # m^2.s^-1 # Variable a déjà prise plus loin
 
 # Géométrie
-Lx = 1
+Lx = 2
 Ly = 1
 Nx = 100
-Ny = Nx
+Ny = 100
 dx = Lx/Nx
 dy = Ly/Ny
 
 # Temps
 Timetot = 1000
-N = 100
+N = 500
 dt = Timetot/N
 
 # Nombres de Fourier
