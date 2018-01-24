946169c36dd0244b1f35a7399beb4d10d60a65bc
Arnaud Charolais <arnaud.charolais@student.ecp.fr>
2018-01-24 14:17:37 +0100

    Small additions: skip writing every file while simulating, postprocessing changed to display more info

--- postproc.py
+++ postproc.py
@@ -10,8 +10,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from analy import analy
 from variables import T1, Ta, h, lamb, Lx, Ly, Nx, Ny
+import math
 
-i = 99
+i = 4999
 Tf = np.genfromtxt('out/output_' + str(i) + '.csv')
 
 Tf = Tf.transpose()
@@ -20,9 +21,10 @@ X = np.arange(Tf.shape[0]) / Nx * Lx
 Y = np.arange(Tf.shape[1]) / Ny * Ly
 
 
-fig, ax1 = plt.subplots(figsize=(12,10))
+fig, ax1 = plt.subplots(figsize=(8,6))
 
 im = ax1.pcolormesh(X,Y,Tf)
+im2 = ax1.contour(X,Y,Tf, colors="k")
 fig.colorbar(im)
 plt.title("Simulation")
 
@@ -36,9 +38,35 @@ X = np.arange(Tanaly.shape[0]) / Nx * Lx
 Y = np.arange(Tanaly.shape[1]) / Ny * Ly
 
 
-fig, ax3 = plt.subplots(figsize=(12,10))
+fig, ax3 = plt.subplots(figsize=(8,6))
 
 im = ax3.pcolormesh(X,Y,Tanaly)
+im2 = ax3.contour(X,Y,Tanaly, colors="k")
 fig.colorbar(im)
 plt.title("Analytic")
+
+# Erreur finale
+Erreursim = Tf - Tanaly
+fig, ax4 = plt.subplots(figsize=(8,6))
+im = ax4.pcolormesh(X,Y,Erreursim)
+im2 = ax4.contour(X,Y,Erreursim, colors="k")
+fig.colorbar(im)
+plt.title("Erreur")
+
+# Erreur en fonction des itérations
+sz = 5000
+modulo = 100
+yerr = np.zeros(int(sz/modulo))
+count = 0
+for k in range(1,sz): # norme l2
+    if k%modulo != 0 and k != sz-1:
+        continue
+    Temp = np.genfromtxt("out/output_" + str(k) + ".csv").transpose()
+    errtemp = Temp - Tanaly
+    yerr[count] = math.sqrt(np.sum(np.square(errtemp)))
+    count += 1
+
+xerr = np.arange(len(yerr))
+fig, ax5 = plt.subplots(figsize=(8,6))
+ax5.plot(xerr,yerr)
 
